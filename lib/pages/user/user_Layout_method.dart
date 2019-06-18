a446c8b58943f8cadb7f2e826da3dffda23541e9
shencang <2630610009@qq.com>
2019-06-18 21:58:46 +0800

    高斯模糊用户头像

---
+++ lib/pages/user/user_Layout_method.dart
@@ -0,0 +1,12 @@
+import 'dart:typed_data';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
+import 'package:image/image.dart' as gImage;
+//
+//class LayoutMethod {
+//  Future<Uint8List> gaussianBlur() async{
+//    gImage.Image image = gImage.Image.from(Image.asset(name));
+//
+//  }
+//}
--- lib/pages/user/userpage.dart
+++ lib/pages/user/userpage.dart
@@ -1,17 +1,23 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_todo_list/utils/app_constant.dart';
 import 'package:flutter_todo_list/utils/app_util.dart';
 
 ///用户界面
 class UserScreen extends StatefulWidget {
-
   @override
-  _UserScreen createState() =>new _UserScreen();
-
-
+  _UserScreen createState() => new _UserScreen();
 }
 
 class _UserScreen extends State<UserScreen> {
+  Image userHead = new Image.asset(
+    "assets/profile_pics.jpg",
+    width: MediaQueryData.fromWindow(window).size.width,
+    height: 300,
+    fit: BoxFit.fitHeight,
+  );
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -26,7 +32,44 @@ class _UserScreen extends State<UserScreen> {
             children: <Widget>[
               Card(
                 child: Column(
-                  children: <Widget>[],
+                  children: <Widget>[
+                    Stack(
+                      children: <Widget>[
+                        userHead,
+                        BackdropFilter(
+                          filter: new ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                          child: new Container(
+                            color: Colors.white.withOpacity(0.1),
+                            width: MediaQueryData.fromWindow(window).size.width,
+                            height: 300,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: <Widget>[
+                                Image.asset(
+                                  "assets/profile_pics.jpg",
+                                  width: 200,
+                                  fit: BoxFit.fitWidth,
+                                ),
+                                Padding(
+                                    padding: EdgeInsets.only(top: 20),
+                                    child: Text(
+                                      "用户名",
+                                      style: TextStyle(fontSize: 30),
+                                    )),
+                                Padding(
+                                    padding: EdgeInsets.only(top: 10),
+                                    child: Text(
+                                      "用户的个性签名",
+                                      style: TextStyle(fontSize: 20),
+                                    ))
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
               ),
             ],
