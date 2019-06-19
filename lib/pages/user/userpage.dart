7c53c1d4128d0da9e8e7fe0e52b51a9bdae775c7
shencang <2630610009@qq.com>
2019-06-19 18:02:12 +0800

    高斯模糊用户头像

--- lib/pages/user/userpage.dart
+++ lib/pages/user/userpage.dart
@@ -11,12 +11,21 @@ class UserScreen extends StatefulWidget {
 }
 
 class _UserScreen extends State<UserScreen> {
-  Image userHead = new Image.asset(
+  Image userHeadBlur = new Image.asset(
     "assets/profile_pics.jpg",
-    width: MediaQueryData.fromWindow(window).size.width,
+    width: MediaQueryData
+        .fromWindow(window)
+        .size
+        .width,
     height: 300,
     fit: BoxFit.fitHeight,
   );
+  Image userHead = new Image.asset("assets/profile_pics.jpg");
+
+  getUserHead(){
+    return "assets/profile_pics.jpg";
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -30,52 +39,72 @@ class _UserScreen extends State<UserScreen> {
           padding: const EdgeInsets.all(8.0),
           child: ListView(
             children: <Widget>[
-              Card(
+          Card(
+          child: Column(
+            children: <Widget>[
+            Stack(
+            children: <Widget>[
+            userHeadBlur,
+            BackdropFilter(
+              filter: new ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: new Container(
+                color: Colors.white.withOpacity(0.1),
+                width: MediaQueryData
+                    .fromWindow(window)
+                    .size
+                    .width,
+                height: 300,
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: <Widget>[
-                    Stack(
-                      children: <Widget>[
-                        userHead,
-                        BackdropFilter(
-                          filter: new ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                          child: new Container(
-                            color: Colors.white.withOpacity(0.1),
-                            width: MediaQueryData.fromWindow(window).size.width,
-                            height: 300,
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: <Widget>[
-                                Image.asset(
-                                  "assets/profile_pics.jpg",
-                                  width: 200,
-                                  fit: BoxFit.fitWidth,
-                                ),
-                                Padding(
-                                    padding: EdgeInsets.only(top: 20),
-                                    child: Text(
-                                      "用户名",
-                                      style: TextStyle(fontSize: 30),
-                                    )),
-                                Padding(
-                                    padding: EdgeInsets.only(top: 10),
-                                    child: Text(
-                                      "用户的个性签名",
-                                      style: TextStyle(fontSize: 20),
-                                    ))
-                              ],
-                            ),
-                          ),
+                    Container(
+                      width: 120.0,
+                      height: 120.0,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage("assets/profile_pics.jpg")
                         ),
-                      ],
+                      ),
                     ),
+                    Padding(
+                        padding: EdgeInsets.only(top: 20),
+                        child: Text(
+                          "用户名",
+                          style: TextStyle(fontSize: 30),
+                        )),
+                    Padding(
+                        padding: EdgeInsets.only(top: 10),
+                        child: Text(
+                          "用户的个性签名",
+                          style: TextStyle(fontSize: 20),
+                        ))
                   ],
                 ),
               ),
+            ),
             ],
           ),
+          ],
         ),
       ),
+      Card(
+        child: Padding(
+          padding: EdgeInsets.only(top: 10),
+          child: Column(
+          children: <Widget>[
+          Text("测试测试测试"),
+
+          ],
+        ),
+      ),
+
+    )],
+    ),
+    ),
+    ),
     );
-  }
+    }
 }
